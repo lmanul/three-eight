2775572a37a87cf82f57960b3b7d31f907178162
Manu Cornet <m@ma.nu>
2025-02-19 00:53:19 +0000

    Parse the word list

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express()
 const port = 3000
 
+const wordsRaw = fs.readFileSync('data/words.txt',
+    { encoding: 'utf8', flag: 'r' }
+);
+const words = wordsRaw.split('\n');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Loading...');
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
