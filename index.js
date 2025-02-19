bb56a9321cf2d0e1ba651f8d2bfc9de407339167
Manu Cornet <m@ma.nu>
2025-02-19 01:46:49 +0000

    Minimal autocomplete container

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const wordsRaw = fs.readFileSync('data/words.txt',
 );
 const words = wordsRaw.split('\n');
 
-app.use('/', express.static('public'));
+app.get('/s/:queryText', (request, response) => {
+    const queryText = request.params.queryText;
+    const matches = [];
+    for (let word of words) {
+        if (word.includes(queryText)) {
+            matches.push(word);
+        }
+    }
+    response.send(matches.join('|'));
+});
 
+app.use('/', express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
